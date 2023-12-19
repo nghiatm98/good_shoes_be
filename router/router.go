083dcbb7f4f9ffc1afdb6bd4c95d73a2badf3571
nghiatm98 <trungtt@smartosc.com>
2023-12-20 00:15:24 +0700

    Update router middleware

--- router/router.go
+++ router/router.go
@@ -1,17 +1,21 @@
 package router
 
 import (
-    "gorm.io/gorm"
+    "log"
     "net/http"
     "time"
 
+    "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
     "go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
     "go.opentelemetry.io/otel"
     "go.opentelemetry.io/otel/trace"
     "golang.org/x/net/context"
+    "gorm.io/gorm"
 
     "good_shoes/common/config"
+    mediaService "good_shoes/media/service"
+    orderService "good_shoes/order/service"
     productService "good_shoes/product/service"
 )
 
@@ -37,7 +41,7 @@ var tracer trace.Tracer
 
 func init() {
     // Name the tracer after the package, or the service if you are in main
-    tracer = otel.Tracer("git.onepay.vn/onepay/go/ddsp/router")
+    tracer = otel.Tracer("router")
 }
 
 // NewServer creates new server instance
@@ -71,14 +75,66 @@ func (server *Server) setupRoute() {
     //gin.SetMode(gin.ReleaseMode)
     router := gin.Default()
     router.Use(otelgin.Middleware("router"))
+    apiPrefix := server.config.ApiPrefix
 
-    // product router
-    productConfig := server.config.ProductConfig
+    // setup CORS
+    config := cors.Config{
+        AllowAllOrigins: true,
+        AllowMethods:    []string{"GET", "POST", "PUT", "DELETE"},
+        AllowHeaders:    []string{"Origin", "Content-Type", "Authorization"},
+    }
+    router.Use(cors.New(config))
+
+    // add jwt middleware
+    authMiddleware, err := initJwt()
+    if err != nil {
+        log.Fatal("JWT Error:" + err.Error())
+    }
+    if errInit := authMiddleware.MiddlewareInit(); nil != errInit {
+        log.Fatal("authMiddleware.MiddlewareInit() Error:" + errInit.Error())
+    }
+
+    router.POST(apiPrefix+"/v1/login", authMiddleware.LoginHandler)
+
+    authRouter := router.Group("")
+    authRouter.Use(authMiddleware.MiddlewareFunc())
+
+    // user
+    authRouter.GET(apiPrefix+"/v1/user", func(c *gin.Context) {
+        user := c.Query("user")
+
+        if user == "admin" {
+            c.JSON(http.StatusOK, gin.H{
+                "name":  "Admin",
+                "email": "admin@good-shoes.tr29.store",
+                "image": "https://www.good-shoes.tr29.store/uploads/1702918213333424600_user.png",
+            })
+            return
+        }
+
+        c.JSON(http.StatusBadRequest, gin.H{
+            "message": "User not found",
+        })
+    })
+
+    // product module
     productHandler, _ := productService.NewHandler(&server.config, server.database, tracer)
-    router.POST(productConfig.ApiPrefix+"/v1/products", productHandler.CreateProduct)
-    router.PUT(productConfig.ApiPrefix+"/v1/products/:id", productHandler.UpdateProduct)
-    router.GET(productConfig.ApiPrefix+"/v1/products", productHandler.ListProduct)
-    router.GET(productConfig.ApiPrefix+"/v1/products/:id", productHandler.GetProduct)
+    router.GET(apiPrefix+"/v1/products", productHandler.ListProduct)
+    router.GET(apiPrefix+"/v1/products/:id", productHandler.GetProduct)
+    authRouter.POST(apiPrefix+"/v1/products", productHandler.CreateProduct)
+    authRouter.PUT(apiPrefix+"/v1/products/:id", productHandler.UpdateProduct)
+    authRouter.DELETE(apiPrefix+"/v1/products/:id", productHandler.DeleteProduct)
+
+    // sales order module
+    orderHandler, _ := orderService.NewHandler(&server.config, server.database, tracer)
+    router.POST(apiPrefix+"/v1/orders", orderHandler.CreateSalesOrder)
+    router.GET(apiPrefix+"/v1/orders", orderHandler.ListSalesOrder)
+    router.GET(apiPrefix+"/v1/orders/:id", orderHandler.GetSalesOrder)
+    authRouter.PUT(apiPrefix+"/v1/orders/:id/status", orderHandler.UpdateSalesOrderStatus)
+
+    // upload image
+    mediaHandler, _ := mediaService.NewHandler(&server.config, tracer)
+    authRouter.POST(apiPrefix+"/v1/uploads", mediaHandler.UploadMultipleFile)
 
     server.router = router
 }
